830cfdb02957d1ad66eecfabddabda2004f84292
Darpan Madlani <Darpan@proclink.com>
2022-02-19 20:58:41 +0530

    part time wage calculation is added

--- src/com/bridgelabz/employee/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employee/EmployeeWageBuilder.java
@@ -3,8 +3,9 @@ package com.bridgelabz.employee;
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
-		final int PRESENT_NUMBER_RANGE = 2;
+		final int PRESENT_NUMBER_RANGE = 3;
 		final int PRESENT = 1;
+		final int PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
 		final int WORKING_HOUR = 8;
 
@@ -14,7 +15,10 @@ public class EmployeeWageBuilder {
 		if (isPresent == PRESENT) {
 			System.out.println("Employee is present");
 			empWage = WAGE_PER_HOUR * WORKING_HOUR;
-		} else {
+		} else if(isPresent == PART_TIME) {
+			System.out.println("Employee is working part time");
+			empWage = WAGE_PER_HOUR * (WORKING_HOUR/2);
+		}else {
 			System.out.println("Employee is absent");
 			empWage = 0;
 		}
