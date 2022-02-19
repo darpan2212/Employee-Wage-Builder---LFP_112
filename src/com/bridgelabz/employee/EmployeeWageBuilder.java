ff348d0211177c427208a9033ec876d92da27ff6
Darpan Madlani <Darpan@proclink.com>
2022-02-19 20:29:51 +0530

    calculating the emp wage based on attendance

--- src/com/bridgelabz/employee/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employee/EmployeeWageBuilder.java
@@ -3,17 +3,22 @@ package com.bridgelabz.employee;
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
+		final int PRESENT_NUMBER_RANGE = 2;
 		final int PRESENT = 1;
+		final int WAGE_PER_HOUR = 20;
+		final int WORKING_HOUR = 8;
 
-		int isPresent = (int) Math.floor(Math.random() * 2);
+		int isPresent = (int) Math.floor(Math.random() * PRESENT_NUMBER_RANGE);
 
-		System.out.println(isPresent);
+		int empWage;
 		if (isPresent == PRESENT) {
 			System.out.println("Employee is present");
+			empWage = WAGE_PER_HOUR * WORKING_HOUR;
 		} else {
 			System.out.println("Employee is absent");
+			empWage = 0;
 		}
-
+		System.out.println("Emp wage : $" + empWage + " USD");
 	}
 
 }
